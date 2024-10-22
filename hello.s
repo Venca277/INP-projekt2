47cc4f5b44c955aad9f2795fb85b98db404b81e2
Václav Sovák <venca03@centrum.cz>
2024-10-22 22:33:10 +0200

    added instruction ddiv, slti for less than zero, mfhi remain and fixed overflow, underflow

--- hello.s
+++ hello.s
@@ -19,43 +19,65 @@ params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
                 .text
 
 main:           ; ZDE NAHRADTE KOD VASIM RESENIM
-                daddi   r4, r0, msg ; vozrovy vypis: adresa msg do r4
-                daddi   r5, r0, cipher
-                daddi   r6, r0, key ; adresa klice do r6
-                daddi   r7, r0, 3   ; delka klice
-                daddi   r8, r0, 0   ; pocitadlo cyklu
-                daddi   r9, r0, 0   ; index na klici
-                daddi   r10, r0, 0  ; pricteni 0/odecteni = 1
-                daddi   r11, r0, 26 ; hodnota preteceni
+                daddi   r3, r0, msg ; vozrovy vypis: adresa msg do r4
+                daddi   r4, r0, cipher
+                daddi   r5, r0, key ; adresa klice do r6
+                daddi   r6, r0, 3   ; delka klice
+                daddi   r7, r0, 0   ; pocitadlo cyklu
+                daddi   r8, r0, 0   ; index na klici
+                daddi   r9, r0, 0  ; pricteni 0/odecteni = 1
+                daddi   r10, r0, 26 ; hodnota preteceni
+
+                daddi   r11, r0, 0
+                daddi   r12, r0, 0
+                daddi   r13, r0, 0
+                daddi   r15, r0, 1 ; odd even check
+                daddi   r16, r0, 0
 
 encrypt_loop:
-                lb r12, 0(r4)
-                beqz r12, end
+                lb      r11, 0(r3)          ; Load current character from msg into r11
+                beqz    r11, end            ; If the character is null (end of string), exit loop
 
-                lb r13, 0(r6)
+                lb      r12, 0(r5)          ; Load current key character into r12
 
-                sub r12, r12, 97    ;konvertujeme char na pozici v abecede 0...25(a...z)
-                sub r13, r13, 97    ;konvertujeme char na pozici v abecede 0...25(a...z)
+                daddi     r11, r11, -96        ; Convert msg character to alphabet index (msg[i] - 'a')
+                daddi     r12, r12, -96        ; Convert key character to alphabet index (key[j] - 'a')
 
-                add r15, r12, r13   ;charakter posunuty o sifru v r15
+                beqz    r15, minus
 
-                div r15, r15, 26    ;konvertuje preteceni na index v abecede
-                add r15, r15, 97
+                daddi   r15, r0, 0
+                add     r13, r11, r12       ; Add msg character and key character indices
 
-                sb r15, 0(r5)
+                j after
+minus:
+                daddi   r15, r0, 1
+                dsub    r13, r11, r12
 
-                daddi r4, r4, 1
-                daddi r5, r5, 1
+after:
+                ddiv    r13, r10            ; Divide by 26 (apply modulo 26)
+                mfhi    r13                 ; Get remainder from division (r13 % 26)
 
-                daddi r9, r9, 1
 
-                div r9, r7
-                mfhi r9
 
-                daddi r6, r0, key
-                dadd  r6, r6, r9
+                slti    r16, r13, 1         ; check if r13 is less than 0
+                beqz    r16, no_adding
+                daddi   r13, r13, 26
+no_adding:
+                daddi   r13, r13, 96        ; Convert back to ASCII (wrap within alphabet)
+                sb      r13, 0(r4)          ; Store ciphered character in the cipher array
 
-                j encrypt_loop
+                daddi   r3, r3, 1           ; Move to the next character in msg
+                daddi   r4, r4, 1           ; Move to the next character in cipher
+
+                daddi   r8, r8, 1           ; Increment the key index
+
+                ddiv    r8, r6              ; r8 = r8 % r6 (wrap the key index)
+                mfhi    r8                  ; Get the remainder (r8 % r6)
+
+                daddi   r5, r0, key         ; Reset key pointer to the start of the key
+                dadd    r5, r5, r8          ; Adjust key pointer based on the wrapped key index
+
+                j       encrypt_loop        ; Repeat for the next character
 
 end:
                 daddi r4, r0, cipher
