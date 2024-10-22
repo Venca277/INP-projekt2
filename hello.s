f4982b96e11c5d3040b58636855af5679e74fc69
Václav Sovák <venca03@centrum.cz>
2024-10-22 23:47:26 +0200

    added proper comments

--- hello.s
+++ hello.s
@@ -1,4 +1,4 @@
-; Autor reseni: jmeno prijmeni login
+; Autor reseni: Václav Sovák xsovakv00
 
 ; Projekt 2 - INP 2024
 ; Vigenerova sifra na architekture MIPS64
@@ -19,69 +19,62 @@ params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
                 .text
 
 main:           ; ZDE NAHRADTE KOD VASIM RESENIM
-                daddi   r3, r0, msg ; vozrovy vypis: adresa msg do r4
-                daddi   r4, r0, cipher
-                daddi   r5, r0, key ; adresa klice do r6
-                daddi   r6, r0, 3   ; delka klice
-                daddi   r7, r0, 0   ; pocitadlo cyklu
-                daddi   r8, r0, 0   ; index na klici
-                daddi   r9, r0, 0  ; pricteni 0/odecteni = 1
-                daddi   r10, r0, 26 ; hodnota preteceni
+                daddi   r3, r0, msg     ; registr pro zpravu k zasifrovani (vaclavsovak)
+                daddi   r4, r0, cipher  ; registr pro budouci sifru
+                daddi   r5, r0, key     ; registr pro klic (sov)
+                daddi   r6, r0, 3       ; delka klice
+                daddi   r8, r0, 0       ; ukazatel na char v klici
+                daddi   r9, r0, 0       ; nahradni registr
+                daddi   r10, r0, 26     ; dylka abecedy
 
-                daddi   r11, r0, 0
-                daddi   r12, r0, 0
-                daddi   r13, r0, 0
-                daddi   r15, r0, 1 ; odd even check
-                daddi   r16, r0, 0
+                daddi   r11, r0, 0      ; misto pro prvni charakter ze zpravy
+                daddi   r12, r0, 0      ; misto pro prvni charakter z klice
+                daddi   r13, r0, 0      ; misto pro posunuty charakter o klic
+                daddi   r15, r0, 1      ; skace mezi 0 & 1 podle toho se rozhoduje odcitani nebo scitani
+                daddi   r16, r0, 0      ; registr pro kontrolu intervalu (-∞ ; 0˃
 
 encrypt_loop:
-                lb      r11, 0(r3)          ; Load current character from msg into r11
-                beqz    r11, end            ; If the character is null (end of string), exit loop
+                lb      r11, 0(r3)              ; nacte charakter ukazatele na zpravu
+                beqz    r11, end                ; pokud je charakter 0, ukonci cyklus
+                lb      r12, 0(r5)              ; nacte charakter ukazatele z klice
 
-                lb      r12, 0(r5)          ; Load current key character into r12
+                daddi     r11, r11, -96         ; prevede pismeno z ascii hodnoty na 1 - 26
+                daddi     r12, r12, -96         ; to same pro charakter z klice 1 - 26
 
-                daddi     r11, r11, -96        ; Convert msg character to alphabet index (msg[i] - 'a')
-                daddi     r12, r12, -96        ; Convert key character to alphabet index (key[j] - 'a')
+                beqz    r15, minus              ; pokud je registr (soucet | rozdil) nula, preskoci na krok minus (bude se odcitat)
+                daddi   r15, r0, 0              ; pokud neskoci tak nastavi do registru nulu, aby se priste skocilo
+                add     r13, r11, r12           ; secte klic a zpravu dohromady a ulozi do r13
 
-                beqz    r15, minus
-
-                daddi   r15, r0, 0
-                add     r13, r11, r12       ; Add msg character and key character indices
-
-                j after
+                j after                         ; skoci na after (vyhne se rozdilu)
 minus:
-                daddi   r15, r0, 1
-                dsub    r13, r11, r12
+                daddi   r15, r0, 1              ; nastavi registr (soucet | rozdil) na 1 (bude se scitat)
+                dsub    r13, r11, r12           ; odecte klic od zpravy a ulozi do r13
 
 after:
-                ddiv    r13, r10            ; Divide by 26 (apply modulo 26)
-                mfhi    r13                 ; Get remainder from division (r13 % 26)
+                ddiv    r13, r10                ; modulo 26 pro detekci preteceni
+                mfhi    r13                     ; ziska zbytek po deleni z registru HI
 
-
-
-                slti    r16, r13, 1         ; check if r13 is less than 0
-                beqz    r16, no_adding
-                daddi   r13, r13, 26
+                slti    r16, r13, 1             ; rozhodne (ANO|NE)(1|0) jestli je hodnota v r13 mensi nez 1 (-∞ ; 0˃
+                beqz    r16, no_adding          ; pokud je v r16 nula tak preskoci pricteni 26
+                daddi   r13, r13, 26            ; neskocil, takze se pricte 26
 no_adding:
-                daddi   r13, r13, 96        ; Convert back to ASCII (wrap within alphabet)
-                sb      r13, 0(r4)          ; Store ciphered character in the cipher array
+                daddi   r13, r13, 96            ; prevede hodnotu sifrovaneho charakteru na ascii hodnotu
+                sb      r13, 0(r4)              ; ulozi hodnotu do registru r4
 
-                daddi   r3, r3, 1           ; Move to the next character in msg
-                daddi   r4, r4, 1           ; Move to the next character in cipher
+                daddi   r3, r3, 1               ; inkrementuje ukazatel na dalsi charakter ve zprave
+                daddi   r4, r4, 1               ; inkrementuje ukazatel na dalsi charakter sifry
+                daddi   r8, r8, 1               ; inkrementuje ukazatel klice
 
-                daddi   r8, r8, 1           ; Increment the key index
+                ddiv    r8, r6                  ; provede modulo klice s delkou klice
+                mfhi    r8                      ; zbytek ulozi do r8
 
-                ddiv    r8, r6              ; r8 = r8 % r6 (wrap the key index)
-                mfhi    r8                  ; Get the remainder (r8 % r6)
-
-                daddi   r5, r0, key         ; Reset key pointer to the start of the key
-                dadd    r5, r5, r8          ; Adjust key pointer based on the wrapped key index
-
-                j       encrypt_loop        ; Repeat for the next character
+                daddi   r5, r0, key             ; nejprve resetujeme ukazatel na klic
+                dadd    r5, r5, r8              ; nacte zbytek jako ukazatel na klic (zajisten cyklismus)
+                j       encrypt_loop            ; skoci na pocatek cyklu (zpracovava dalsi charakter)
 
 end:
-                daddi r4, r0, cipher
-                jal     print_string ; vypis pomoci print_string - viz nize
+                daddi   r4, r0, cipher          ; ukazatel na zacatek sifry
+                jal     print_string            ; vypis pomoci print_string - viz nize
 
 ; NASLEDUJICI KOD NEMODIFIKUJTE!
 
